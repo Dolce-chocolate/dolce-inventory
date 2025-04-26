b70f21a918a56d2bc8dd462ef2cd7a169f92def1
Khairi Khalil <khayri472@gmail.com>
2025-04-26 20:11:51 +0200

    🔗 إصلاح رابط إضافة منتج وربط جميع الأزرار

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -13,48 +13,56 @@ export default function AdminDashboard() {
       </h1>
 
       <div className="grid grid-cols-3 gap-6 justify-items-center">
+        {/* Chocolate Store */}
         <Link href="/chocolate">
           <div className="w-24 h-24 bg-[#f5e8dc] rounded-xl flex flex-col items-center justify-center shadow hover:brightness-95">
             <img src="/choco1.jpeg" alt="Chocolate" className="w-8 h-8" />
           </div>
         </Link>
 
+        {/* Pack Store */}
         <Link href="/packs">
           <div className="w-24 h-24 bg-[#f5e8dc] rounded-xl flex flex-col items-center justify-center shadow hover:brightness-95">
             <img src="/pack1.jpeg" alt="Pack" className="w-8 h-8" />
           </div>
         </Link>
 
+        {/* Product Search */}
         <Link href="/search">
           <div className="w-24 h-24 bg-[#f5e8dc] rounded-xl flex flex-col items-center justify-center shadow hover:brightness-95">
             <img src="/ffd1.jpeg" alt="Search" className="w-8 h-8" />
           </div>
         </Link>
 
+        {/* Coffee Store */}
         <Link href="/cafe">
           <div className="w-24 h-24 bg-[#f5e8dc] rounded-xl flex flex-col items-center justify-center shadow hover:brightness-95">
             <img src="/cofe1.jpeg" alt="Coffee" className="w-8 h-8" />
           </div>
         </Link>
 
+        {/* User Management */}
         <Link href="/admin-dashboard/manage-users">
           <div className="w-24 h-24 bg-[#f5e8dc] rounded-xl flex flex-col items-center justify-center shadow hover:brightness-95">
-            <img src="/manage.jpeg" alt="Manage" className="w-8 h-8" />
+            <img src="/manage.jpeg" alt="Manage Users" className="w-8 h-8" />
           </div>
         </Link>
 
+        {/* Reports */}
         <Link href="/reports">
           <div className="w-24 h-24 bg-[#f5e8dc] rounded-xl flex flex-col items-center justify-center shadow hover:brightness-95">
             <img src="/reports1.jpeg" alt="Reports" className="w-8 h-8" />
           </div>
         </Link>
 
+        {/* Add Product */}
         <Link href="/add">
           <div className="w-24 h-24 bg-[#f5e8dc] rounded-xl flex flex-col items-center justify-center shadow hover:brightness-95">
             <img src="/add1.jpeg" alt="Add Product" className="w-8 h-8" />
           </div>
         </Link>
 
+        {/* Logout */}
         <button
           onClick={() => {
             localStorage.clear();
